2e988aa2e66bcdba34ba51a2d33014ad404c70c7
Thang Pham <qcthangph@outlook.com>
2024-07-24 19:29:04 +0700

    new changes include adding a new cosine function

--- test1.py
+++ test1.py
@@ -6,8 +6,12 @@ x = np.arange(0, 5 * np.pi, 0.1)
 y = np.sin(x)
 y2 = x
 
+x3 = np.arange(0, 5 * np.pi, 0.1)
+y3 = np.cos(x3)
+
 # Step 2: Plot the sine graph
 plt.plot(x, y, color='green')
+plt.plot(x3, y3, color='red')
 plt.xlabel('x')
 plt.ylabel('sin(x)')
 plt.title('Sine Curve using Matplotlib')
